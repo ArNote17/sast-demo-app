a24ebd91ceb69d6f4c480211af07b27849cbb25f
ArNote17 <96125371+ArNote17@users.noreply.github.com>
2024-11-26 00:36:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ import shlex
 def run_command(cmd):
     safe_cmd = shlex.split(cmd)
     subprocess.call(safe_cmd)
+
+if __name__ == "__main__":
+    cmd = input("Enter a command to run: ")
+    run_command(cmd)
